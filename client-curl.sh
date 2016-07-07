3d277016d00192f2f72d1531446256a4aa14c4aa
jhink7 <jhink7@gmail.com>
2016-07-06 21:37:40 -0700

    Added second Curl Command for demo purposes

--- client-curl.sh
+++ client-curl.sh
@@ -1,1 +1,3 @@
 curl -H "Content-Type: application/json" -X POST -d '{"features":[0.0,29.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,2.0,1.0,0.0,0.0,0.0,0.0,0.5,1.0,0.0,10.0,3.0,0.3,0.3,0.3,0.0,0.0,0.0,0.0,0.0]}' http://localhost:8080/traffic/block
+
+curl -H "Content-Type: application/json" -X POST -d '{"path":"data/corrected.gz"}' http://localhost:8080/traffic/reload
